0474b1c2023c0a1985d24bb6e41dfd3577c51890
nawealvarez <37459045+nawealvarez@users.noreply.github.com>
2018-09-14 01:28:27 -0300

    .

--- billeterapp2.0/presentacion/__init__.py
+++ billeterapp2.0/presentacion/__init__.py
@@ -5,4 +5,4 @@ from flask_moment import Moment
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'oS\xf8\xf4\xe2\xc8\xda\xe3\x7f\xc75*\x83\xb1\x06\x8c\x85\xa4\xa7piE\xd6I'
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
